ab001f21b77b38742b4494711ca8ecc477509c95
tiagocmendes <tiagocmendes@ua.pt>
2020-03-12 21:50:15 +0000

    React router

--- mylife/src/App.js
+++ mylife/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+// import layouts
+import Home from './layouts/Home';
+import Login from './layouts/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const hist = createBrowserHistory();
+
+    return (
+        <div id="app">
+            <Router history={hist}>
+                <Switch>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ mylife/src/layouts/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div id="home">
+            <h1>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
