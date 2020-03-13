01036622d150756120b1f6f9f86fe83fdd1ee415
tiagocmendes <tiagocmendes@ua.pt>
2020-03-13 16:46:31 +0000

    Converted elate template to React

--- mylife/src/components/Home/Intro/Intro.js
+++ mylife/src/components/Home/Intro/Intro.js
@@ -1,27 +1,48 @@
 import React from 'react';
 
-import whiteLogo from '../../../assets/home/white-logo.png'
-
 function Intro() {
+
     return (
         <div id="intro">
-            <section id="fh5co-home" data-section="home" data-stellar-background-ratio="0.5">
-                <div class="gradient"></div>
+            <section id="fh5co-intro">
                 <div class="container">
-                    <div class="text-wrap">
-                        <div class="text-inner">
-                            <div class="row">
-                                <div class="col-md-8 col-md-offset-2">
-                                    <img class="to-animate" style={{marginTop: "-200px", width: "250px", height:"250px"}} src={whiteLogo}/>
-                                    <h1 class="to-animate">Be the best version of yourself</h1>
-                                    <h2 class="to-animate">With our personal lifestyle monitoring system,</h2>
-                                    <h2 class="to-animate">your life will never be the same!</h2>
-                                </div>
+                    <div class="row row-bottom-padded-lg">
+                        <div class="fh5co-block to-animate">
+                            <div class="overlay-darker"></div>
+                            <div class="overlay"></div>
+                            <div class="fh5co-text">
+                                <i class="fh5co-intro-icon icon-bulb"></i>
+                                <h2>Plan</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                                <p><a href="#" class="btn btn-primary">Get In Touch</a></p>
+                            </div>
+                        </div>
+                        <div class="fh5co-block to-animate" >
+                            <div class="overlay-darker"></div>
+                            <div class="overlay"></div>
+                            <div class="fh5co-text">
+                                <i class="fh5co-intro-icon icon-wrench"></i>
+                                <h2>Develop</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                                <p><a href="#" class="btn btn-primary">Click Me</a></p>
+                            </div>
+                        </div>
+                        <div class="fh5co-block to-animate">
+                            <div class="overlay-darker"></div>
+                            <div class="overlay"></div>
+                            <div class="fh5co-text">
+                                <i class="fh5co-intro-icon icon-rocket"></i>
+                                <h2>Launch</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                                <p><a href="#" class="btn btn-primary">Why Us?</a></p>
                             </div>
                         </div>
                     </div>
+                    <div class="row watch-video text-center to-animate">
+                        <span>Watch the video</span>
+                        <a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo btn-video"><i class="icon-play2"></i></a>
+                    </div>
                 </div>
-                <div class="slant"></div>
             </section>
         </div>
     );
