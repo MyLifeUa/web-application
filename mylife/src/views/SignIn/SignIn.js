980e164ece16cf8d4499c79d616277972806e581
tiagocmendes <tiagocmendes@ua.pt>
2020-03-21 18:28:16 +0000

    Finished login layout

--- mylife/src/views/SignIn/SignIn.js
+++ mylife/src/views/SignIn/SignIn.js
@@ -19,7 +19,15 @@ import {
 
 import Header from '../../components/SignIn/Header';
 
+import logo from '../../assets/home/white-logo.png'
+
 class SignIn extends React.Component {
+
+    inputStyle = {
+        borderColor: "#4ca2d1",
+        textColor: "#4ca2d1"
+    }
+
     render() {
         return (
             <div>
@@ -29,25 +37,39 @@ class SignIn extends React.Component {
                         <div class="limiter">
                             <div class="container-login100">
                                 <div class="wrap-login100 p-t-50 p-b-90">
-                                    <h1 class="h1-title">Sign In</h1>
+                                    <h1 class="h1-title"><i class="fas fa-heartbeat"></i><br />Sign In</h1>
                                     <Form>
                                         <Row>
-                                            <Col md="1"></Col>
-                                            <Col md="10">
+                                            <Col sm="1"></Col>
+                                            <Col sm="10">
                                                 <FormGroup>
-                                                    <InputGroup classname="mb-4">
-                                                        <InputGroupAddon addonType="prepend">
-                                                            <InputGroupText>
-                                                                <i class="ni ni-zoom-split-in"></i>
-                                                            </InputGroupText>
-                                                        </InputGroupAddon>
-                                                        <Input placeholder="Email" type="email" />
-                                                    </InputGroup>
+                                                    <Input style={this.inputStyle} placeholder="Email" type="email" />
                                                 </FormGroup>
                                             </Col>
-                                            <Col md="1"></Col>
+                                            <Col sm="1"></Col>
+                                        </Row>
+                                        <Row>
+                                            <Col sm="1"></Col>
+                                            <Col sm="10">
+                                                <FormGroup>
+                                                    <Input style={this.inputStyle} placeholder="Password" type="password" />
+                                                </FormGroup>
+                                            </Col>
+                                            <Col sm="1"></Col>
+                                        </Row>
+                                        <Row>
+                                            <Col sm="1"></Col>
+                                            <Col sm="10">
+                                                <Button block color="info" size="lg" type="button">Sign In</Button>
+                                            </Col>
+                                            <Col sm="1"></Col>
                                         </Row>
                                     </Form>
+                                    <div class="row">
+                                        <div class="col-sm-1"></div>
+                                        <div class="col-sm-10 login-forget-pw"><a href="#">Forgot password?</a></div>
+                                        <div class="col-sm-1"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
