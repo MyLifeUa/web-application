5d9055eedebb29801bd123fe3703d7b4aa0527a3
tiagocmendes <tiagocmendes@ua.pt>
2020-04-09 14:40:57 +0100

    Add patiet layout

---
+++ mylife/src/views/Doctor/Patients/AddPatient.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AddPatient extends React.Component {
+
+
+    render() {
+        return (
+            <h1>Add patient</h1>
+        )
+    }
+
+}
+
+export default AddPatient;
