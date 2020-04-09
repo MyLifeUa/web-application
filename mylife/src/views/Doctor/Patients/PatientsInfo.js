7763b9d992d80faee1340dd26e4c9dbfad311218
tiagocmendes <tiagocmendes@ua.pt>
2020-04-09 04:43:13 +0100

    Added stats table

--- mylife/src/views/Doctor/Patients/PatientsInfo.js
+++ mylife/src/views/Doctor/Patients/PatientsInfo.js
@@ -7,14 +7,31 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
+import IconButton from '@material-ui/core/IconButton';
+import Table from "components/Table/Table.js";
+import ArrowBack from '@material-ui/icons/ArrowBackIos';
+
+
+import Patients from "views/Doctor/Patients/Patients.js";
 
 class PatientsInfo extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            currentPatient: props.currentPatient
+            currentPatient: props.currentPatient,
+            return: false,
+            stats: [
+                props.currentPatient.weight_goal
+            ]
         }
+        this.toggleReturn = this.toggleReturn.bind(this);
+    }
+
+    toggleReturn() {
+        this.setState({
+            return: true
+        })
     }
 
     classes = {
@@ -41,14 +58,15 @@ class PatientsInfo extends React.Component {
 	};
 
     render() {
+        if(this.state.return) return <Patients />
         return (
             <div>
                 <GridContainer>
                     <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-50px", marginBottom: "20px" }}>
-                        <IconButton aria-label="delete">
-                            <EyeIcon onClick={() => this.showDetails(patient.email)} style={{ color: "#00acc1" }} fontSize="medium" />
-                        </IconButton>,
-                        <h3>Patient details</h3>
+                    <h3><IconButton aria-label="back">
+                            <ArrowBack onClick={() => this.toggleReturn()} style={{ color: "#00acc1" }} fontSize="medium" />
+                        </IconButton>
+                        Patient details</h3>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={4}>
                         <Card profile>
@@ -72,9 +90,16 @@ class PatientsInfo extends React.Component {
                     <GridItem xs={12} sm={12} md={8}>
                         <Card>
                             <CardHeader style={this.classes.cardHeader}>
-                            <h4 style={this.classes.cardTitleWhite}><i className="fas fa-heartbeat"></i>  Health measures</h4>
+                                <h4 style={this.classes.cardTitleWhite}><i className="fas fa-heartbeat"></i>  Health measures</h4>
                                 <p style={this.classes.cardCategoryWhite}>Overview of patient vital signs</p>
                             </CardHeader>
+                            <CardBody>
+                                <Table
+                                    tableHeaderColor="info"
+                                    tableHead={["", "Measure", "Value"]}
+                                    tableData={this.state.stats}
+                                />
+                            </CardBody>
                         </Card>
                     </GridItem>
                 </GridContainer>
