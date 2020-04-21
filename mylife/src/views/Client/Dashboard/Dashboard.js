89fa342c4f1bb37471c1192060bfb6e2b37fdf71
tiagocmendes <tiagocmendes@ua.pt>
2020-04-21 15:45:10 +0100

    Small fix

--- mylife/src/views/Client/Dashboard/Dashboard.js
+++ mylife/src/views/Client/Dashboard/Dashboard.js
@@ -18,10 +18,10 @@ class Dashboard extends React.Component {
             <div id="client-dashboard">
                 <GridContainer>
                     <GridItem xs={12} sm={12} md={1}>
-                        <img style={{ height: "40px", borderRadius: "50%" }} src={"data:image;base64," + this.authUser.message.photo} />
+                        <img style={{ height: "40px", borderRadius: "50%" }} src={"data:image;base64," + this.authUser.message.photo} alt={this.authUser.message.name}/>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={11} style={{ marginTop: "-20px", marginLeft: "-30px" }}>
-                        <h3> Welcome, <a>{this.authUser.message.name}!</a></h3>
+                        <h3> Welcome, <a href="/client/profile">{this.authUser.message.name}!</a></h3>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-20px"}}>
                         <h4><i className="fas fa-calendar-alt" style={{color: "#00acc1"}}></i> {utils.weekday[this.today.getDay()]}, {this.today.getUTCDate()}th of {utils.monthNames[this.today.getMonth()] + " " + this.today.getFullYear()}</h4>
