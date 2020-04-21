7af7092c60cba08291d6ee49f317e411d3ada3a9
tiagocmendes <tiagocmendes@ua.pt>
2020-04-21 23:21:30 +0100

    Started body history

--- mylife/src/variables/baseURI.js
+++ mylife/src/variables/baseURI.js
@@ -9,6 +9,7 @@ export default {
         patients: 'http://mednat.ieeta.pt:8442/doctor-clients',
         patientAssociation: 'http://mednat.ieeta.pt:8442/doctor-patient-association',
         checkEmail: 'http://mednat.ieeta.pt:8442/check-email/',
-        nutrientsHistory: 'http://mednat.ieeta.pt:8442/health-stats/nutrients/history/'
+        nutrientsHistory: 'http://mednat.ieeta.pt:8442/health-stats/nutrients/history/',
+        bodyHistory: 'http://mednat.ieeta.pt:8442/health-stats/body/history/'
     }
 }
--- mylife/src/views/Client/History/History.js
+++ mylife/src/views/Client/History/History.js
@@ -25,8 +25,10 @@ class History extends React.Component {
     constructor() {
         super();
         this.state = {
-            nutrient: {name: "Calories", period: "week"},
-            nutrientsHistory: utils.defaultHistory
+            nutrient: { name: "Calories", period: "week" },
+            nutrientsHistory: utils.defaultHistory,
+            body: { name: "Calories", period: "week" },
+            bodyHistory: utils.defaultHistory
         }
 
         this.nutrientsCache = {}
@@ -36,23 +38,37 @@ class History extends React.Component {
             });
         });
 
+        this.bodyCache = {}
+        utils.body.forEach(b => {
+            utils.periods.forEach(period => {
+                this.bodyCache[b + "" + period] = [];
+            });
+        });
+
         this.authUser = JSON.parse(localStorage.getItem('authUser'));
+
         this.toggleNutrient = this.toggleNutrient.bind(this);
         this.toggleNutrientPeriod = this.toggleNutrientPeriod.bind(this);
         this.fetchNutrients = this.fetchNutrients.bind(this);
+        this.toggleBody = this.toggleBody.bind(this);
+        this.toggleBodyPeriod = this.toggleBodyPeriod.bind(this);
+        this.fetchBody = this.fetchBody.bind(this);
     }
 
     componentDidMount() {
         this.fetchNutrients(this.state.nutrient.name, this.state.nutrient.period);
+        this.fetchBody(this.state.body.name, this.state.body.period);
     }
 
 
     fetchNutrients(nutrient, period) {
 
-        if(this.nutrientsCache[nutrient + "" + period].length !== 0) {
+        if (this.nutrientsCache[nutrient + "" + period].length !== 0) {
             this.setState({
-                nutrient: {name: nutrient, period: period},
+                nutrient: { name: nutrient, period: period },
                 nutrientsHistory: this.nutrientsCache[nutrient + "" + period],
+                body: this.state.body,
+                bodyHistory: this.state.bodyHistory,
             })
             return;
         }
@@ -74,8 +90,10 @@ class History extends React.Component {
                 this.authUser.token = data.token;
                 localStorage.setItem('authUser', JSON.stringify(this.authUser));
                 this.setState({
-                    nutrient: {name: nutrient, period: period},
+                    nutrient: { name: nutrient, period: period },
                     nutrientsHistory: data.message.history,
+                    body: this.state.body,
+                    bodyHistory: this.state.bodyHistory,
                 })
                 this.nutrientsCache[nutrient + "" + period] = data.message.history;
 
@@ -83,7 +101,7 @@ class History extends React.Component {
             .catch(error => {
                 console.log("Fetch error: " + error);
             })
-        
+
     }
 
 
@@ -95,6 +113,57 @@ class History extends React.Component {
         this.fetchNutrients(this.state.nutrient.name, event.target.value);
     }
 
+
+    fetchBody(body, period) {
+
+        if (this.bodyCache[body + "" + period].length !== 0) {
+            this.setState({
+                nutrient: this.state.nutrient,
+                nutrientsHistory: this.state.nutrientsHistory,
+                body: { name: body, period: period },
+                bodyHistory: this.bodyCache[body + "" + period],
+            })
+            return;
+        }
+
+
+        fetch(baseUri.restApi.bodyHistory + this.authUser.message.email + "?metric=" + body.toLowerCase() + "&period=" + period, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Token " + this.authUser.token
+            }
+        })
+            .then(response => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
+            })
+            .then(data => {
+                this.authUser.token = data.token;
+                localStorage.setItem('authUser', JSON.stringify(this.authUser));
+                this.setState({
+                    nutrient: this.state.nutrient,
+                    nutrientsHistory: this.state.nutrientsHistory,
+                    body: { name: body, period: period },
+                    bodyHistory: data.message.history,
+                })
+                this.bodyCache[body + "" + period] = data.message.history;
+
+            })
+            .catch(error => {
+                console.log("Fetch error: " + error);
+            })
+
+    }
+
+    toggleBody = (event) => {
+        this.fetchBody(event.target.value, this.state.body.period);
+    }
+
+    toggleBodyPeriod = (event) => {
+        this.fetchBody(this.state.body.name, event.target.value);
+    }
 
     render() {
         return (
@@ -143,6 +212,58 @@ class History extends React.Component {
                             width={1000}
                             height={300}
                             data={this.state.nutrientsHistory}
+                            margin={{
+                                top: 5, right: 30, left: 5, bottom: 20,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="day" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="value" stroke="#00acc1" activeDot={{ r: 8 }} />
+                        </LineChart>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={6}>
+                        <h4><i className="fas fa-dumbbell" style={{ color: "#00acc1", marginRight: "5px" }}></i> Body history - <strong>{this.state.nutrient.name}</strong></h4>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={3} style={{ marginTop: "15px" }}>
+                        <FormControl variant="outlined" style={{ width: "180px" }}>
+                            <InputLabel id="demo-simple-select-outlined-label">Metric</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-outlined-label"
+                                id="nutrients-metric"
+                                value={this.state.body.name}
+                                onChange={this.toggleBody}
+                                label="Metric"
+                            >
+                                {utils.body.map((b, key) => {
+                                    return <MenuItem value={b}>{b}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={3} style={{ marginTop: "15px" }}>
+                        <FormControl variant="outlined" style={{ width: "180px" }}>
+                            <InputLabel id="demo-simple-select-outlined-label">Period</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-outlined-label"
+                                id="nutrients-period"
+                                value={this.state.body.period}
+                                onChange={this.toggleBodyPeriod}
+                                label="Period"
+                            >
+                                {utils.periods.map((period, key) => {
+                                    return <MenuItem value={period}>{period}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={12}>
+                        <LineChart
+                            width={1000}
+                            height={300}
+                            data={this.state.bodyHistory}
                             margin={{
                                 top: 5, right: 30, left: 5, bottom: 20,
                             }}
