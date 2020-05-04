70f04971f1da59a2c1f31c00d17a1c101873ab2a
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-04 21:50:50 +0100

    Legend colors done

--- mylife/src/views/Client/History/History.js
+++ mylife/src/views/Client/History/History.js
@@ -10,7 +10,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 
@@ -27,7 +28,24 @@ class History extends React.Component {
             body: { name: "Calories", period: "week" },
             bodyHistory: utils.defaultHistory,
             heartSegmentStops: [49, 62, 66, 75, 82, 95],
-            heartRate: 0
+            heartRate: 0,
+            heartLabels: [],
+            myLifeRate: 0,
+            myLifeLabels: []
+        };
+
+        this.heartColors = {
+            "Excellent": "#76E880",
+            "Good": "#99FF33",
+            "Average": "#99FFFF",
+            "Fair": "#80CCFF",
+            "Poor": "#BB99FF"
+        };
+
+        this.myLifeColors = {
+            "Poor":"#0FA3B1",
+            "Average":"#B5E2FA",
+            "Excellent":"#F7A072",
         }
 
         this.nutrientsCache = {}
@@ -53,12 +71,14 @@ class History extends React.Component {
         this.toggleBodyPeriod = this.toggleBodyPeriod.bind(this);
         this.fetchBody = this.fetchBody.bind(this);
         this.fetchHeart = this.fetchHeart.bind(this);
+        this.fetchMyLifeLabel = this.fetchMyLifeLabel.bind(this);
     }
 
     componentDidMount() {
+        this.fetchMyLifeLabel();
+        this.fetchHeart();
         this.fetchNutrients(this.state.nutrient.name, this.state.nutrient.period);
         this.fetchBody(this.state.body.name, this.state.body.period);
-        this.fetchHeart();
     }
 
 
@@ -130,20 +150,60 @@ class History extends React.Component {
 
             })
             .then(data => {
-
                 let heartSegmentStops = [];
 
+                let heartLabels = [];
+
                 for (let key in data.message.scale) {
+                    heartLabels.push(
+                        <div style={{ padding: "5px", height: "30px", width: "120px", backgroundColor: this.heartColors[data.message.scale[key]] }}><strong style={{ color: "white" }}>{key}: {data.message.scale[key]}</strong></div>
+                    )
                     key = key + "-";
                     heartSegmentStops.push(parseInt(String(key).split("-")[0]))
+
                 }
                 heartSegmentStops.push(99);
-                console.log(heartSegmentStops.sort())
+
                 this.setState({
                     heartSegmentStops: heartSegmentStops.sort(),
-                    heartRate: data.message.avg_heart_rate <= 99 ? data.message.avg_heart_rate : 99
+                    heartRate: data.message.avg_heart_rate <= 99 ? data.message.avg_heart_rate : 99,
+                    heartLabels: heartLabels.sort()
                 })
 
+            })
+            .catch(error => {
+                console.log("Fetch error: " + error);
+            })
+    }
+
+    fetchMyLifeLabel() {
+        fetch(baseUri.restApi.myLifeLabel + this.authUser.message.email, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Token " + this.authUser.token
+            }
+        })
+            .then(response => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
+
+            })
+            .then(data => {
+                let myLifeLabels = [];
+
+                for (let key in data.message.scale) {
+                    myLifeLabels.push(
+                        <div style={{ padding: "5px", height: "30px", width: "120px", backgroundColor: this.myLifeColors[data.message.scale[key]] }}><strong style={{ color: "white" }}>{key}: {data.message.scale[key]}</strong></div>
+                    )
+                }
+                console.log(data.message.current_week.value);
+                console.log(myLifeLabels);
+                this.setState({
+                    myLifeLabels: myLifeLabels,
+                    myLifeRate: data.message.current_week.value
+                })
             })
             .catch(error => {
                 console.log("Fetch error: " + error);
@@ -197,7 +257,7 @@ class History extends React.Component {
                         goal: data.message.goal !== undefined ? data.message.goal : 0
                     })
                 })
-                
+
                 this.setState({
                     nutrient: this.state.nutrient,
                     nutrientsHistory: this.state.nutrientsHistory,
@@ -229,7 +289,99 @@ class History extends React.Component {
                         <h3><i className="fas fa-file-medical-alt" style={{ color: "#00acc1", marginRight: "5px" }}></i> Health History</h3>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={6}>
-                        <h4><i className="fas fa-apple-alt" style={{ color: "#00acc1", marginRight: "5px" }}></i> Nutrients history - <strong>{this.state.nutrient.name}</strong></h4>
+                        <h4><i className="fas fa-heartbeat" style={{ color: "#00acc1", marginRight: "5px" }}></i> <strong>MyLife Metric</strong></h4>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={6}>
+                        <h4><i className="fas fa-heartbeat" style={{ color: "#00acc1", marginRight: "5px" }}></i> <strong>Heart history</strong></h4>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={4}>
+                        {this.state.heartRate !== 0 &&
+                            <ReactSpeedometer
+                                minValue={0}
+                                value={this.state.myLifeRate}
+                                maxValue={5}
+                                currentValueText={this.state.myLifeRate + ""}
+                                customSegmentStops={[0, 2, 4, 5]}
+                                segmentColors={[
+                                    "#0FA3B1",
+                                    "#B5E2FA",
+                                    "#F7A072",
+                                ]}
+                                customSegmentLabels={[
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                ]}
+                            />}
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={2}>
+                        <GridContainer>
+                            {this.state.myLifeLabels.map(label => {
+                                return <GridItem xs={12} sm={12} md={12} style={{ marginBottom: "10px" }}>
+                                    {label}
+                                </GridItem>
+                            })}
+                        </GridContainer>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={4}>
+                        {this.state.heartRate !== 0 &&
+                            <ReactSpeedometer
+                                minValue={this.state.heartSegmentStops[0]}
+                                value={this.state.heartRate}
+                                maxValue={this.state.heartSegmentStops[this.state.heartSegmentStops.length - 1]}
+                                currentValueText={this.state.heartRate + " bpm"}
+                                customSegmentStops={this.state.heartSegmentStops}
+                                segmentColors={[
+                                    "#76E880",
+                                    "#99FF33",
+                                    "#99FFFF",
+                                    "#80CCFF",
+                                    "#BB99FF",
+                                ]}
+                                customSegmentLabels={[
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                    {
+                                        position: "INSIDE",
+                                        color: "white",
+                                    },
+                                ]}
+                            />}
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={2}>
+                        <GridContainer>
+                            {this.state.heartLabels.map(label => {
+                                return <GridItem xs={12} sm={12} md={12} style={{ marginBottom: "10px" }}>
+                                    {label}
+                                </GridItem>
+                            })}
+                        </GridContainer>
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={6}>
+                        <h4><i className="fas fa-apple-alt" style={{ color: "#00acc1", marginRight: "5px" }}></i> <strong>Nutrients history</strong> - {this.state.nutrient.name}</h4>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={3} style={{ marginTop: "15px" }}>
                         <FormControl variant="outlined" style={{ width: "180px" }}>
@@ -282,7 +434,7 @@ class History extends React.Component {
                         </LineChart>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={6}>
-                        <h4><i className="fas fa-dumbbell" style={{ color: "#00acc1", marginRight: "5px" }}></i> Body history - <strong>{this.state.body.name}</strong></h4>
+                        <h4><i className="fas fa-dumbbell" style={{ color: "#00acc1", marginRight: "5px" }}></i> <strong>Body history</strong> - {this.state.body.name}</h4>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={3} style={{ marginTop: "15px" }}>
                         <FormControl variant="outlined" style={{ width: "180px" }}>
@@ -334,52 +486,7 @@ class History extends React.Component {
                             <Line type="monotone" dataKey="goal" stroke="red" />
                         </LineChart>
                     </GridItem>
-                    <GridItem xs={12} sm={12} md={12} style={{ color: "#00acc1" }}><h6>Heart Rate</h6></GridItem>
 
-                    <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-30px" }}>
-                        {this.state.heartRate !== 0 &&
-                            <ReactSpeedometer
-                                minValue={this.state.heartSegmentStops[0]}
-                                value={this.state.heartRate}
-                                maxValue={this.state.heartSegmentStops[this.state.heartSegmentStops.length - 1]}
-                                currentValueText={this.state.heartRate + " bpm"}
-                                customSegmentStops={this.state.heartSegmentStops}
-                                segmentColors={[
-                                    "#00FF65",
-                                    "#60E065",
-                                    "#FFE71A",
-                                    "#ffa21a",
-                                    "#f44336",
-                                ]}
-                                customSegmentLabels={[
-                                    {
-                                        text: "Excellent",
-                                        position: "INSIDE",
-                                        color: "white",
-                                    },
-                                    {
-                                        text: "Good",
-                                        position: "INSIDE",
-                                        color: "white",
-                                    },
-                                    {
-                                        text: "Average",
-                                        position: "INSIDE",
-                                        color: "white",
-                                    },
-                                    {
-                                        text: "Fair",
-                                        position: "INSIDE",
-                                        color: "white",
-                                    },
-                                    {
-                                        text: "Poor",
-                                        position: "INSIDE",
-                                        color: "white",
-                                    },
-                                ]}
-                            />}
-                    </GridItem>
 
                 </GridContainer>
             </div>
