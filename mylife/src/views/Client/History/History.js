4618bad98b6114f118987ce983952d9dc701b2dd
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-05 04:59:04 +0100

    Remove unused variables

--- mylife/src/views/Client/History/History.js
+++ mylife/src/views/Client/History/History.js
@@ -10,7 +10,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-import Card from "components/Card/Card.js";
+import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 
 import baseUri from "variables/baseURI";
