54396e9ca35960884719740d4db4c44cffa3a92f
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-07 19:15:43 +0100

    Add meal types field

--- mylife/src/views/Client/FoodLogs/Insert.js
+++ mylife/src/views/Client/FoodLogs/Insert.js
@@ -8,6 +8,10 @@ import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
 import { DatePickerInput } from 'rc-datepicker';
 import ArrowBack from '@material-ui/icons/ArrowBackIos';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 import FoodLogs from "views/Client/FoodLogs/FoodLogs.js";
 
@@ -27,6 +31,7 @@ class Insert extends React.Component {
         this.state = {
             return: false,
             date: new Date(),
+            mealType: "breakfast"
         }
 
         this.authUser = JSON.parse(localStorage.getItem('authUser'));
@@ -34,6 +39,7 @@ class Insert extends React.Component {
         this.date = this.state.date.toISOString().split('T')[0];
 
         this.toggleReturn = this.toggleReturn.bind(this);
+        this.changeTypeMeal = this.changeTypeMeal.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
@@ -43,11 +49,16 @@ class Insert extends React.Component {
         })
     }
 
+    changeTypeMeal(event) {
+        alert(event.target.value);
+    }
+
     onChange = (jsDate, dateString) => {
         document.getElementById("birth-date").value = dateString.split("T")[0];
         this.setState({
             date: new Date(dateString.split("T")[0])
         })
+        alert(document.getElementById("meal-type").value)
     }
 
     render() {
@@ -69,9 +80,9 @@ class Insert extends React.Component {
                             <CardBody>
                                 <GridContainer>
                                     <GridItem xs={12} sm={12} md={1}>
-                                        <h4>Day: </h4>
+                                        <h4 style={{ color: "#00acc1" }}>DATE: </h4>
                                     </GridItem>
-                                    <GridItem xs={12} sm={12} md={3}>
+                                    <GridItem xs={12} sm={12} md={3} style={{ marginTop: "18px" }}>
                                         <DatePickerInput
                                             locale="en-SG"
                                             id="birth-date"
@@ -81,21 +92,28 @@ class Insert extends React.Component {
                                     </GridItem>
                                     <GridItem xs={12} sm={12} md={8}></GridItem>
                                     <GridItem xs={12} sm={12} md={2}>
-                                        <h4>Type of meal: </h4>
+                                        <h4 style={{ color: "#00acc1" }}>TYPE OF MEAL: </h4>
                                     </GridItem>
-                                    <GridItem xs={12} sm={12} md={3}>
-                                        <h4>Day: </h4>
+                                    <GridItem xs={12} sm={12} md={5} style={{marginTop: "20px"}}>
+                                        <FormControl component="fieldset">
+                                            <RadioGroup row aria-label="position" name="position" defaultValue={this.state.mealType}>
+                                                <FormControlLabel value="breakfast" control={<Radio color="primary" onChange={this.changeTypeMeal} />} label="Breakfast" />
+                                                <FormControlLabel value="lunch" control={<Radio color="primary" onChange={this.changeTypeMeal} />} label="Lunch" />
+                                                <FormControlLabel value="snack" control={<Radio color="primary" onChange={this.changeTypeMeal} />} label="Snack" />
+                                                <FormControlLabel value="dinner" control={<Radio color="primary" onChange={this.changeTypeMeal} />} label="Dinner" />
+                                            </RadioGroup>
+                                        </FormControl>
                                     </GridItem>
-                                    <GridItem xs={12} sm={12} md={7}></GridItem>
+                                    <GridItem xs={12} sm={12} md={5}></GridItem>
                                     <GridItem xs={12} sm={12} md={3}>
-                                        <h4>Number of serving: </h4>
+                                        <h4 style={{ color: "#00acc1" }}>SERVINGS: </h4>
                                     </GridItem>
                                     <GridItem xs={12} sm={12} md={3}>
                                         <h4>Day: </h4>
                                     </GridItem>
                                     <GridItem xs={12} sm={12} md={6}></GridItem>
                                     <GridItem xs={12} sm={12} md={1}>
-                                        <h4>Meal: </h4>
+                                        <h4 style={{ color: "#00acc1" }}>MEAL: </h4>
                                     </GridItem>
                                     <GridItem xs={12} sm={12} md={3}>
                                         <h4>Day: </h4>
