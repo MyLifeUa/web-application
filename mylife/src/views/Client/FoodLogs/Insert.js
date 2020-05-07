5959eeb49decfbf24722f798b8dfb4c3f18a3996
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-07 01:35:33 +0100

    Food logs routing done

---
+++ mylife/src/views/Client/FoodLogs/Insert.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import IconButton from '@material-ui/core/IconButton';
+import Table from "components/Table/Table.js";
+import ArrowBack from '@material-ui/icons/ArrowBackIos';
+
+import FoodLogs from "views/Client/FoodLogs/FoodLogs.js";
+
+class Insert extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            return: false
+        }
+
+        this.toggleReturn = this.toggleReturn.bind(this);
+    }
+
+    toggleReturn() {
+        this.setState({
+            return: true
+        })
+    }
+
+    render() {
+        if(this.state.return) return <FoodLogs />
+        return(
+            <div id="food-logs">
+                <GridContainer>
+                    <GridItem xs={12} sm={12} md={12} style={{ marginTop: "-50px" }}>
+                        <h3><IconButton aria-label="back">
+                            <ArrowBack onClick={() => this.toggleReturn()} style={{ color: "#00acc1" }} fontSize="medium" />
+                        </IconButton>
+                        Insert new food log</h3>
+                    </GridItem>
+                </GridContainer>
+            </div>
+        )
+    }
+}
+
+export default Insert;
