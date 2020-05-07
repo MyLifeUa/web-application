a3ba58c5dd0b98b377d17208603fa25d3c9db092
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-07 23:39:52 +0100

    Removed unused variables

--- mylife/src/views/Client/FoodLogs/List.js
+++ mylife/src/views/Client/FoodLogs/List.js
@@ -8,8 +8,6 @@ import DeleteForeverIcon from '@material-ui/icons/DeleteForever.js';
 import Button from "components/CustomButtons/Button.js";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 
